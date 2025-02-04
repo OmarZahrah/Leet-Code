ae1d84ba56ad8301bc7834ce069fd7b0a8944cc4
OmarZahrah <omar.zahrah01@gmail.com>
2025-02-04 19:05:07 +0200

    rename folder

--- NeetCode/1- Arrays & Hashing/1-Contains Duplicate/217-Contains-Duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        Map={}
-        for num in nums:
-            Map[num]=Map.get(num,0)+1
-            if Map[num]>1:
-                return True
-        
-        return False
--- NeetCode/1- Arrays & Hashing/1-Contains Duplicate/217-Contains-Duplicate.ts
+++
@@ -1,8 +0,0 @@
-function containsDuplicate(nums: number[]): boolean {
-    const freqs={};
-    for(let num of nums){
-        freqs[num]=1+(freqs[num]|0);
-        if(freqs[num]>1)return true;
-    }
-    return false;
-};
