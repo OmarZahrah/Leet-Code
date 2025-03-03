fae7a091fec656cede0830c9af12ec9114e7a8dd
Omar Zahrah <120282274+OmarZahrah@users.noreply.github.com>
2025-03-03 06:19:46 +0200

    [2161] [Time Beats: 68.33%] [Memory Beats: 30.00%] - LeetPush

--- Random/Arrays/Partition-Array-According-to-Given-Pivot.ts
+++ Random/Arrays/Partition-Array-According-to-Given-Pivot.ts
@@ -1,32 +1,38 @@
-// function topKFrequent(nums: number[], k: number): void {
-//   const count: { [key: number]: number } = {};
+// function pivotArray(nums: number[], pivot: number): number[] {
+//   const less: number[] = [];
+//   const pivots:number[]=[];
+//   const greater: number[] = [];
 
 //   for (const num of nums) {
-//     count[num] = (count[num] || 0) + 1;
+//     if (num < pivot) less.push(num);
+//     else if (num > pivot) greater.push(num);
+//     else pivots.push(num);
 //   }
-
-//   const arr = Object.entries(count).map(([num, freq]) => [freq, parseInt(num)]);
-//   arr.sort((a, b) => b[0] - a[0]);
-//   console.log(arr);
-// }
-
-// const nums = [1, 1, 1, 2, 2, 3];
-// const k = 2;
-
-// console.log(topKFrequent(nums, k));
-
-function pivotArray(nums: number[], pivot: number): number[] {
-  const less: number[] = [];
-  const pivots:number[]=[];
-  const greater: number[] = [];
-
-  for (const num of nums) {
-    if (num < pivot) less.push(num);
-    else if (num > pivot) greater.push(num);
-    else pivots.push(num);
-  }
   
 
-  return [...less,...pivots,...greater];
+//   return [...less,...pivots,...greater];
+// }
+
+function pivotArray(nums: number[], pivot: number): number[] {
+   const n = nums.length;
+    const result: number[] = new Array(n);
+    let left = 0 , pivots = 0, greater = 0;
+
+    for (const num of nums) {
+        if (num < pivot) left++;
+        else if (num === pivot) pivots++;
+    }
+    
+    let middle=left;
+    let right=middle+pivots;
+    left=0;
+
+    for (const num of nums) {
+        if (num < pivot) result[left++] = num;
+        else if (num === pivot) result[middle++] = num;
+        else result[right++] = num;
+    }
+    return result;
+
 }
 
